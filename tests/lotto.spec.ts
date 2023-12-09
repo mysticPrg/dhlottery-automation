630c48b1b592f74860836a6d70634b11a9e74f0b
mysticPrg <mysticPrg@gmail.com>
2023-12-09 14:06:06 +0900

    iframe 대기시간 추가

--- tests/lotto.spec.ts
+++ tests/lotto.spec.ts
@@ -58,6 +58,8 @@ function getRandomNumbers() {
 async function buyLotto(page: Page, amount: number, smoke: boolean) {
     const frame = page.frameLocator('#ifrm_tab');
     const numbers = getRandomNumbers()
+    
+    page.waitForTimeout(2000);
 
     for (const num of numbers) {
         await frame.locator(`label[for="check645num${num}"]`).click();
