39f022580e65f5a6b9c0c9dcc487070793f37da0
mysticPrg <mysticPrg@gmail.com>
2023-12-10 06:12:21 +0900

    iframe 사용 제거

--- tests/lotto.spec.ts
+++ tests/lotto.spec.ts
@@ -1,48 +1,22 @@
 import { test, expect, Page } from '@playwright/test'
+import { login, waitLoading } from './utils'
 
-test.describe('복권 구매 자동화', () => {
-  test.describe.configure({
-    mode: 'serial',
-  })
+test('로또 구매', async ({ page }) => {
+  const id = process.env.USER_ID
+  const password = process.env.USER_PW
+  const amount = process.env.AMOUNT
+  const smoke = process.env.SMOKE === 'true'
+  if (!id || !password || !amount) {
+    return
+  }
 
-  test('로또 구매', async ({ page }) => {
-    const id = process.env.USER_ID
-    const password = process.env.USER_PW
-    const amount = process.env.AMOUNT
-    const smoke = process.env.SMOKE === 'true'
-    if (!id || !password || !amount) {
-      return
-    }
+  expect(id).not.toBeFalsy()
+  expect(password).not.toBeFalsy()
+  expect(parseInt(amount)).not.toBeNaN()
 
-    expect(id).not.toBeFalsy()
-    expect(password).not.toBeFalsy()
-    expect(parseInt(amount)).not.toBeNaN()
-
-    await login(page, id, password)
-    await buyLotto(page, parseInt(amount), smoke)
-  })
+  await login(page, id, password)
+  await buyLotto(page, parseInt(amount), smoke)
 })
-
-async function login(page: Page, id: string, password: string) {
-  await page.goto('https://dhlottery.co.kr/user.do?method=login&returnUrl=')
-
-  await waitLoading(page)
-
-  const idInput = page.locator('#userId')
-  await idInput.focus()
-  await idInput.fill(id)
-
-  await page.keyboard.press('Tab')
-  const passwordInput = page.locator('*:focus')
-  await passwordInput.fill(password)
-
-  await page.keyboard.press('Tab')
-  const loginButton = page.locator('*:focus')
-  await loginButton.click()
-
-  await page.waitForTimeout(1000)
-  await waitLoading(page)
-}
 
 function getRandomNumbers() {
   return Array.from({ length: 45 })
@@ -53,31 +27,22 @@ function getRandomNumbers() {
 }
 
 async function buyLotto(page: Page, amount: number, smoke: boolean) {
-  await page.goto('https://el.dhlottery.co.kr/game/TotalGame.jsp?LottoId=LO40')
+  await page.goto('https://ol.dhlottery.co.kr/olotto/game/game645.do')
   await waitLoading(page)
 
-  const frame = page.frameLocator('#ifrm_tab')
   const numbers = getRandomNumbers()
 
-  await page.waitForSelector('#ifrm_tab')
-
   for (const num of numbers) {
-    await frame.locator(`label[for="check645num${num}"]`).click()
+    await page.locator(`label[for="check645num${num}"]`).click()
   }
 
-  await frame.locator('#amoundApply').selectOption(`${amount}`)
+  await page.locator('#amoundApply').selectOption(`${amount}`)
 
-  await frame.locator('#btnSelectNum').click()
-  await frame.locator('#btnBuy').click()
+  await page.locator('#btnSelectNum').click()
+  await page.locator('#btnBuy').click()
 
   const text = smoke ? '취소' : '확인'
-  await frame.locator('#popupLayerConfirm').getByText(text).click()
+  await page.locator('#popupLayerConfirm').getByText(text).click()
 
   await page.waitForTimeout(1000)
-}
-
-async function waitLoading(page: Page) {
-  await page.waitForLoadState('load')
-  await page.waitForLoadState('domcontentloaded')
-  await page.waitForLoadState('networkidle')
 }
---
+++ tests/utils.ts
@@ -0,0 +1,28 @@
+import { Page } from '@playwright/test'
+
+export async function login(page: Page, id: string, password: string) {
+  await page.goto('https://dhlottery.co.kr/user.do?method=login&returnUrl=')
+
+  await waitLoading(page)
+
+  const idInput = page.locator('#userId')
+  await idInput.focus()
+  await idInput.fill(id)
+
+  await page.keyboard.press('Tab')
+  const passwordInput = page.locator('*:focus')
+  await passwordInput.fill(password)
+
+  await page.keyboard.press('Tab')
+  const loginButton = page.locator('*:focus')
+  await loginButton.click()
+
+  await page.waitForTimeout(1000)
+  await waitLoading(page)
+}
+
+export async function waitLoading(page: Page) {
+  await page.waitForLoadState('load')
+  await page.waitForLoadState('domcontentloaded')
+  await page.waitForLoadState('networkidle')
+}
